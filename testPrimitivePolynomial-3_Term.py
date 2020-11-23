c7e7029e3725859c4bebed63ff1ac605fdf4b410
tokatm <33986537+tokatm@users.noreply.github.com>
2020-11-24 01:56:07 +0300

    Add files via upload

---
+++ testPrimitivePolynomial-3_Term.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Test whether primitive polynomial for LFSR construction
+
+
+
+
+import numpy as np
+from sympy import prem
+from sympy.abc import x
+
+
+
+
+
+power1 = int(input('Power1: '))
+power2 = int(input('Power2: '))
+range1 = int(input('Range1: '))
+range2 = int(input('Range2: '))
+
+
+
+
+
+def primitiveTest():
+   
+    for i in range(range1,range2):
+        
+        if prem (x**i + 1, x**power1 + x**power2 + 1, modulus = 2) == 0:
+            
+            print(i)
+
+        else:
+            i + 1
+            print('Not Primitive')
+       
+
+
+
+primitiveTest()
+
